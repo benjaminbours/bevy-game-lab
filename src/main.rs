d3271d9c987e06493bf1872cd3e095f20099585c
Benjamin Bours <boursbenjamin@gmail.com>
2023-04-25 15:45:03 +0100

    Initial commit. Just followed the small Bevy book

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::{
+    app::App,
+    prelude::{Commands, Component, Plugin, Query, Res, ResMut, Resource, With},
+    time::{Time, Timer, TimerMode},
+    DefaultPlugins,
+};
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(great_people);
+    }
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn great_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
